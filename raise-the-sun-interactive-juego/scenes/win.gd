7b28828751fcb8123cb864b30e02ef917a27aac6
Cisterna-O <andres.cisterna@ug.uchile.cl>
2025-12-11 01:16:29 -0300

    agregue final paar el juego

---
+++ raise-the-sun-interactive-juego/scenes/win.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var credits_button: Button = $VBoxContainer/CreditsButton
+@onready var quit_button: Button = $VBoxContainer/QuitButton
+
+func _ready() -> void:
+	credits_button.pressed.connect(_on_credits_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://scenes/nivel_tutorial.tscn")
+	
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+	
+func _on_quit_pressed():
+	get_tree().quit()
+	
